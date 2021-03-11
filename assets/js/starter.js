da32b9f6199073f9737cc3924fd942d3d63970fc
jsmdy <62148532+jsmdy@users.noreply.github.com>
2021-03-11 18:50:12 +0100

    rr

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -7,8 +7,7 @@
 // Option 1
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
-
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 // Option 2
@@ -18,5 +17,5 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+//import "../../node_modules/bootstrap/js/dist/util.js";
+//import "../../node_modules/bootstrap/js/dist/modal.js";
