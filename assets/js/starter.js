533face20f827588d055e364db17f49d94d48b7f
jsmdy <62148532+jsmdy@users.noreply.github.com>
2021-04-21 18:43:07 +0200

    temp
    temp

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -19,3 +19,21 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 //import "../../node_modules/bootstrap/js/dist/util.js";
 //import "../../node_modules/bootstrap/js/dist/modal.js";
+/* parallax */
+
+window.addEventListener('scroll', function (event) {
+    var depth, i, layer, layers, len, movement, topDistance, translate3d;
+    topDistance = this.pageYOffset;
+    layers = document.querySelectorAll("[data-type='parallax']");
+    for (i = 0, len = layers.length; i < len; i++) {
+        layer = layers[i];
+        depth = layer.getAttribute('data-depth');
+        movement = -(topDistance * depth);
+        translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+        layer.style['-webkit-transform'] = translate3d;
+        layer.style['-moz-transform'] = translate3d;
+        layer.style['-ms-transform'] = translate3d;
+        layer.style['-o-transform'] = translate3d;
+        layer.style.transform = translate3d;
+    }
+});
